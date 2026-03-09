faac1c69c78e3666d77b5d95f0bfa87813f6354b
Muhammed Sajil K <mynameissajil@gmail.com>
2026-03-09 22:10:48 +0530

    Update app_library.sql

--- app_library.sql
+++ app_library.sql
@@ -1,86 +1,98 @@
 -- Library System Management SQL Project
 
--- CREATE DATABASE library;
+create database library_project_2;
+use library_project_2;
 
--- Create table "Branch"
-DROP TABLE IF EXISTS branch;
+#------library management system project 2------
+
 CREATE TABLE branch
-(
-            branch_id VARCHAR(10) PRIMARY KEY,
-            manager_id VARCHAR(10),
-            branch_address VARCHAR(30),
-            contact_no VARCHAR(15)
-);
+         (branch_id VARCHAR(10) PRIMARY KEY,
+	      manager_id VARCHAR (20),
+          branch_address VARCHAR (55),
+          contact_no VARCHAR (10));
+ALTER TABLE branch
+MODIFY contact_no VARCHAR(20);
+select * from branch;
 
-
--- Create table "Employee"
-DROP TABLE IF EXISTS employees;
 CREATE TABLE employees
-(
-            emp_id VARCHAR(10) PRIMARY KEY,
-            emp_name VARCHAR(30),
-            position VARCHAR(30),
-            salary DECIMAL(10,2),
-            branch_id VARCHAR(10),
-            FOREIGN KEY (branch_id) REFERENCES  branch(branch_id)
-);
+		(emp_id VARCHAR(10) PRIMARY KEY,
+         emp_name VARCHAR(25),
+         position VARCHAR(15),
+         salary int,
+         branch_id VARCHAR(25));
 
-
--- Create table "Members"
-DROP TABLE IF EXISTS members;
-CREATE TABLE members
-(
-            member_id VARCHAR(10) PRIMARY KEY,
-            member_name VARCHAR(30),
-            member_address VARCHAR(30),
-            reg_date DATE
-);
-
-
-
--- Create table "Books"
-DROP TABLE IF EXISTS books;
+delete from employees;
+	
 CREATE TABLE books
-(
-            isbn VARCHAR(50) PRIMARY KEY,
-            book_title VARCHAR(80),
-            category VARCHAR(30),
-            rental_price DECIMAL(10,2),
-            status VARCHAR(10),
-            author VARCHAR(30),
-            publisher VARCHAR(30)
-);
+	   (isbn VARCHAR(20) PRIMARY KEY,
+        book_title VARCHAR(75),
+        category VARCHAR(10),
+        rental_price FLOAT,
+        status VARCHAR(15),
+        author VARCHAR(35),
+        publisher VARCHAR(55));
+        
+ALTER TABLE books
+MODIFY category VARCHAR(20);
+DELETE FROM books;
+select * from books;
 
-
-
--- Create table "IssueStatus"
-DROP TABLE IF EXISTS issued_status;
+CREATE TABLE members
+       (member_id VARCHAR(10)PRIMARY KEY,
+        member_name VARCHAR(25),
+        member_address VARCHAR(75),
+        reg_date DATE);
+        
 CREATE TABLE issued_status
-(
-            issued_id VARCHAR(10) PRIMARY KEY,
-            issued_member_id VARCHAR(30),
-            issued_book_name VARCHAR(80),
-            issued_date DATE,
-            issued_book_isbn VARCHAR(50),
-            issued_emp_id VARCHAR(10),
-            FOREIGN KEY (issued_member_id) REFERENCES members(member_id),
-            FOREIGN KEY (issued_emp_id) REFERENCES employees(emp_id),
-            FOREIGN KEY (issued_book_isbn) REFERENCES books(isbn) 
-);
-
-
-
--- Create table "ReturnStatus"
-DROP TABLE IF EXISTS return_status;
+       (issued_id VARCHAR(10)PRIMARY KEY,	
+        issued_member_id VARCHAR(10),	
+        issued_book_name VARCHAR(75),	
+        issued_date	DATE, 
+        issued_book_isbn VARCHAR(25),	
+        issued_emp_id VARCHAR(10));
+        
 CREATE TABLE return_status
-(
-            return_id VARCHAR(10) PRIMARY KEY,
-            issued_id VARCHAR(30),
-            return_book_name VARCHAR(80),
-            return_date DATE,
-            return_book_isbn VARCHAR(50),
-            FOREIGN KEY (return_book_isbn) REFERENCES books(isbn)
-);
+       (return_id VARCHAR(10)PRIMARY KEY,	
+        issued_id VARCHAR(10),	
+        return_book_name VARCHAR(10),	
+        return_date DATE,	
+        return_book_isbn VARCHAR(20));
+
+delete from return_status;
+        
+#---FOREIGN KEY----
+
+ALTER TABLE issued_status
+ADD constraint fk_members
+foreign key (issued_member_id)
+REFERENCES members(member_id);
+
+ALTER TABLE issued_status
+ADD constraint fk_books
+foreign key (issued_book_isbn)
+REFERENCES books(isbn);
+
+ALTER TABLE issued_status
+ADD constraint fk_employees
+foreign key (issued_emp_id)
+REFERENCES employees(emp_id);
+
+ALTER TABLE employees
+ADD constraint fk_branch
+foreign key (branch_id)
+REFERENCES branch(branch_id);
+
+ALTER TABLE return_status
+ADD constraint fk_issued_status
+foreign key (issued_id)
+REFERENCES issued_status(issued_id);
+
+DELETE FROM return_status;
+DELETE FROM issued_status;
+DELETE FROM employees;
+DELETE FROM books;
+DELETE FROM members;
+DELETE FROM branch;
 
 
 -- Project TASK
@@ -129,50 +141,4 @@ CREATE TABLE return_status
 -- Task 12: Retrieve the List of Books Not Yet Returned
 
     
-/*
-### Advanced SQL Operations
 
-Task 13: Identify Members with Overdue Books
-Write a query to identify members who have overdue books (assume a 30-day return period). Display the member's name, book title, issue date, and days overdue.
-
-
-Task 14: Update Book Status on Return
-Write a query to update the status of books in the books table to "available" when they are returned (based on entries in the return_status table).
-
-
-
-Task 15: Branch Performance Report
-Create a query that generates a performance report for each branch, showing the number of books issued, the number of books returned, and the total revenue generated from book rentals.
-
-
-Task 16: CTAS: Create a Table of Active Members
-Use the CREATE TABLE AS (CTAS) statement to create a new table active_members containing members who have issued at least one book in the last 6 months.
-
-
-
-Task 17: Find Employees with the Most Book Issues Processed
-Write a query to find the top 3 employees who have processed the most book issues. Display the employee name, number of books processed, and their branch.
-
-
-Task 18: Identify Members Issuing High-Risk Books
-Write a query to identify members who have issued books more than twice with the status "damaged" in the books table. Display the member name, book title, and the number of times they've issued damaged books.    
-
-
-Task 19: Stored Procedure
-Objective: Create a stored procedure to manage the status of books in a library system.
-    Description: Write a stored procedure that updates the status of a book based on its issuance or return. Specifically:
-    If a book is issued, the status should change to 'no'.
-    If a book is returned, the status should change to 'yes'.
-
-Task 20: Create Table As Select (CTAS)
-Objective: Create a CTAS (Create Table As Select) query to identify overdue books and calculate fines.
-
-Description: Write a CTAS query to create a new table that lists each member and the books they have issued but not returned within 30 days. The table should include:
-    The number of overdue books.
-    The total fines, with each day's fine calculated at $0.50.
-    The number of books issued by each member.
-    The resulting table should show:
-    Member ID
-    Number of overdue books
-    Total fines
-*/
